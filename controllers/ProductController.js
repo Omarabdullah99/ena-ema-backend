d6735ace55ab3378fa776d12dcca65b23d0a0d57
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-18 01:09:00 +0600

    fetchProductById

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -13,6 +13,63 @@ export const createProduct = async (req, res) => {
     }
   };
 
+  export const fetchAllProduct = async (req, res) => {
+  
+    let query = ProductModel.find({});
+    let totalProductsQuery = ProductModel.find({});
+  
+    if (req.query.category) {
+      query = query.find({ category: req.query.category });
+      totalProductsQuery = totalProductsQuery.find({
+        category: req.query.category,
+      });
+    }
+  
+    if (req.query.brand) {
+      query = query.find({ brand: req.query.brand });
+      totalProductsQuery = totalProductsQuery.find({
+        brand: req.query.brand,
+      });
+    }
+  
+    //TODo: How to get sort on discounted price not an Actual price
+    if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+    }
+  
+    const totalDocs = await totalProductsQuery.countDocuments().exec();
+    console.log("totalDocs", totalDocs);
+  
+    if (req.query._page && req.query._limit) {
+      const pageSize = req.query._limit;
+      const page = req.query._page;
+      query = query.skip(pageSize * (page - 1)).limit(pageSize);
+    }
+  
+    try {
+      const docs = await query.exec();
+      res.set("X-Total-Count", totalDocs);
+      res.status(200).json(docs);
+    } catch (err) {
+      res.status(400).json(err)
+      console.log("Error of fetallproduct", err);
+    }
+  };
+
+  export const fetchProductById=async(req,res)=>{
+    const {id}= req.params
+    const product= await ProductModel.findById(id)
+    // console.log('signle product',product)
+    try {
+        res.status(200).json(product)
+        
+    } catch (err) {
+        res.status(400).json(err)
+        
+    }
+}
+  
+
   export const testProduct = async(req,res)=>{
     res.send('helloProduct')
   }
--- routers/ProductRouter.js
+++ routers/ProductRouter.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createProduct, testProduct } from '../controllers/ProductController.js'
+import { createProduct, fetchAllProduct, fetchProductById, testProduct } from '../controllers/ProductController.js'
 
 
 const ProductRouter= express.Router()
 
 ProductRouter.get('/testproduct',testProduct)
 ProductRouter.post('/createProduct',createProduct)
+ProductRouter.get('/fetchProducts',fetchAllProduct)
+ProductRouter.get('/productById/:id',fetchProductById)
 
 export default ProductRouter
